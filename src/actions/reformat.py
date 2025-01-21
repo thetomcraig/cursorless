3d3e1d52a2eba79bd5d5b6f3ddb14b8094a2ce52
Tom Craig <thetomcraig@icloud.com>
2025-01-21 13:15:59 -0800

    merge fixes

--- src/actions/reformat.py
+++ src/actions/reformat.py
@@ -11,14 +11,10 @@ mod.list("cursorless_reformat_action", desc="Cursorless reformat action")
 
 @mod.action_class
 class Actions:
-<<<<<<< HEAD:src/actions/reformat.py
-    def private_cursorless_reformat(target: CursorlessTarget, formatters: str):
-=======
     def cursorless_reformat(
         target: CursorlessExplicitTarget,  # pyright: ignore [reportGeneralTypeIssues]
         formatters: str,
     ):
->>>>>>> upstream/main:cursorless-talon/src/actions/reformat.py
         """Execute Cursorless reformat action. Reformat target with formatter"""
         texts = cursorless_get_text_action(target, show_decorations=False)
         updated_texts = [actions.user.reformat_text(text, formatters) for text in texts]
--- src/number_small.py
+++ src/number_small.py
@@ -17,27 +17,4 @@ def on_ready():
         )
 
 
-<<<<<<< HEAD:src/number_small.py
-digit_list = "zero one two three four five six seven eight nine".split()
-teens = "ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split()
-tens = "twenty thirty forty fifty sixty seventy eighty ninety".split()
-
-number_small_list = [*digit_list, *teens]
-for ten in tens:
-    number_small_list.append(ten)
-    number_small_list.extend(f"{ten} {digit}" for digit in digit_list[1:])
-number_small_map = {n: i for i, n in enumerate(number_small_list)}
-
-mod.list("private_cursorless_number_small", desc="List of small numbers")
-ctx.lists["self.private_cursorless_number_small"] = number_small_map.keys()
-
-
-@ctx.capture(
-    "user.private_cursorless_number_small",
-    rule="{user.private_cursorless_number_small}",
-)
-def override_private_cursorless_number_small(m) -> int:
-    return number_small_map[m.private_cursorless_number_small]
-=======
 app.register("ready", on_ready)
->>>>>>> upstream/main:cursorless-talon/src/number_small.py
